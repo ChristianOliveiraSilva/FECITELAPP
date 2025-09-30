0e9ae45ab4ffec92c980c408f4b062d8c670178f
ChristianOliveiraSilva <gamesparamundo123@gmail.com>
2025-09-29 23:34:50 -0400

    update

--- app/build/web/flutter_bootstrap.js
+++ app/build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1e9a811bf8e70466596bcf0ea3a8b5adb5f17f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2485781546"
+    serviceWorkerVersion: "1303178051"
   }
 });
